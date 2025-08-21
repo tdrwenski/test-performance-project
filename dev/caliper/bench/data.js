73096dd297c5bf1474ff6b58313c994c6ff40bb5
github-action-benchmark <github@users.noreply.github.com>
2025-08-21 21:52:03 +0000

    add Benchmark (customSmallerIsBetter) benchmark result for 389b4c1c9d8862ee35d6ee269b3264ca609f7839

---
+++ dev/caliper/bench/data.js
@@ -0,0 +1,38 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1755813123062,
+  "repoUrl": "https://github.com/tdrwenski/test-performance-project",
+  "entries": {
+    "Benchmark": [
+      {
+        "commit": {
+          "author": {
+            "email": "drwenski1@llnl.gov",
+            "name": "Tara Drwenski",
+            "username": "tdrwenski"
+          },
+          "committer": {
+            "email": "drwenski1@llnl.gov",
+            "name": "Tara Drwenski",
+            "username": "tdrwenski"
+          },
+          "distinct": true,
+          "id": "389b4c1c9d8862ee35d6ee269b3264ca609f7839",
+          "message": "Fix permissions",
+          "timestamp": "2025-08-21T15:49:52-06:00",
+          "tree_id": "64c6556910dd94cefa50182479133e7e875080b7",
+          "url": "https://github.com/tdrwenski/test-performance-project/commit/389b4c1c9d8862ee35d6ee269b3264ca609f7839"
+        },
+        "date": 1755813122342,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "Caliper Region - unknown",
+            "value": 56635,
+            "unit": "nanoseconds",
+            "extra": "Invocations: 1\\nTotal time: 56635 nanoseconds"
+          }
+        ]
+      }
+    ]
+  }
+}
