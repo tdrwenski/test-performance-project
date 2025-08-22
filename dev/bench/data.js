10be1521db2427d50c5e61154b5f6b58b3e4f278
github-action-benchmark <github@users.noreply.github.com>
2025-08-22 20:22:38 +0000

    add Benchmark (pytest) benchmark result for a656fea5f473410a29cfe193f9f7cf8d4a4a4c89

---
+++ dev/bench/data.js
@@ -0,0 +1,39 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1755894158104,
+  "repoUrl": "https://github.com/tdrwenski/test-performance-project",
+  "entries": {
+    "Benchmark": [
+      {
+        "commit": {
+          "author": {
+            "email": "drwenski1@llnl.gov",
+            "name": "Tara Drwenski",
+            "username": "tdrwenski"
+          },
+          "committer": {
+            "email": "drwenski1@llnl.gov",
+            "name": "Tara Drwenski",
+            "username": "tdrwenski"
+          },
+          "distinct": true,
+          "id": "a656fea5f473410a29cfe193f9f7cf8d4a4a4c89",
+          "message": "Test putting two benchs in same dir",
+          "timestamp": "2025-08-22T14:21:29-06:00",
+          "tree_id": "3fa594ca70d67676ef0b332d38c4ddba35572b13",
+          "url": "https://github.com/tdrwenski/test-performance-project/commit/a656fea5f473410a29cfe193f9f7cf8d4a4a4c89"
+        },
+        "date": 1755894157288,
+        "tool": "pytest",
+        "benches": [
+          {
+            "name": "pytest/test_script.py::test_simple_computation",
+            "value": 1388.4482493758387,
+            "unit": "iter/sec",
+            "range": "stddev: 0.000049690919383028666",
+            "extra": "mean: 720.2285000175834 usec\nrounds: 10"
+          }
+        ]
+      }
+    ]
+  }
+}
