47891251c60b8c4b0c21f48bd3a18b9f90264a44
github-action-benchmark <github@users.noreply.github.com>
2025-08-22 20:24:15 +0000

    add Benchmark (customSmallerIsBetter) benchmark result for a656fea5f473410a29cfe193f9f7cf8d4a4a4c89

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1755894158104,
+  "lastUpdate": 1755894255510,
   "repoUrl": "https://github.com/tdrwenski/test-performance-project",
   "entries": {
     "Benchmark": [
@@ -31,6 +31,35 @@ window.BENCHMARK_DATA = {
             "unit": "iter/sec",
             "range": "stddev: 0.000049690919383028666",
             "extra": "mean: 720.2285000175834 usec\nrounds: 10"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "drwenski1@llnl.gov",
+            "name": "Tara Drwenski",
+            "username": "tdrwenski"
+          },
+          "committer": {
+            "email": "drwenski1@llnl.gov",
+            "name": "Tara Drwenski",
+            "username": "tdrwenski"
+          },
+          "distinct": true,
+          "id": "a656fea5f473410a29cfe193f9f7cf8d4a4a4c89",
+          "message": "Test putting two benchs in same dir",
+          "timestamp": "2025-08-22T14:21:29-06:00",
+          "tree_id": "3fa594ca70d67676ef0b332d38c4ddba35572b13",
+          "url": "https://github.com/tdrwenski/test-performance-project/commit/a656fea5f473410a29cfe193f9f7cf8d4a4a4c89"
+        },
+        "date": 1755894255069,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "Caliper Function - compute_sum_of_squares",
+            "value": 60103,
+            "unit": "nanoseconds"
           }
         ]
       }
